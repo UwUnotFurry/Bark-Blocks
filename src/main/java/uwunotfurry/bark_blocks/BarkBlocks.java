a144e496372c185753253bb06c105f6ed94ba364
UwUnotFurry <uwunotfurry+GitHub@gmail.com>
2026-02-02 18:10:14 +0000

    Updated for minecraft 1.19.3

--- src/main/java/uwunotfurry/bark_blocks/BarkBlocks.java
+++ src/main/java/uwunotfurry/bark_blocks/BarkBlocks.java
@@ -5,7 +5,6 @@ import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import uwunotfurry.bark_blocks.world.item.Items;
 import uwunotfurry.bark_blocks.world.level.block.Blocks;
 
 public class BarkBlocks implements ModInitializer {
@@ -16,6 +15,5 @@ public class BarkBlocks implements ModInitializer {
 	public void onInitialize() {
 		BarkBlocks.LOGGER.info("Bark Blocks (C) 2026 by UwUnotFurry is licensed under CC BY-NC-ND 4.0. To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-nd/4.0/");
 		Blocks.onInitialise();
-		Items.onInitialise();
 	}
 }
--- src/main/java/uwunotfurry/bark_blocks/world/level/block/Blocks.java
+++ src/main/java/uwunotfurry/bark_blocks/world/level/block/Blocks.java
@@ -2,10 +2,17 @@ package uwunotfurry.bark_blocks.world.level.block;
 
 import java.util.function.Function;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 
 import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
 
 import uwunotfurry.bark_blocks.BarkBlocks;
@@ -69,16 +76,38 @@ public class Blocks {
 	);
 
 	public static void onInitialise() {
-		FlammableBlockRegistry.getDefaultInstance().add(OAK_BARK, 30, 60);
-		FlammableBlockRegistry.getDefaultInstance().add(SPRUCE_BARK, 30, 60);
-		FlammableBlockRegistry.getDefaultInstance().add(BIRCH_BARK, 30, 60);
-		FlammableBlockRegistry.getDefaultInstance().add(JUNGLE_BARK, 30, 60);
-		FlammableBlockRegistry.getDefaultInstance().add(ACACIA_BARK, 30, 60);
-		FlammableBlockRegistry.getDefaultInstance().add(DARK_OAK_BARK, 30, 60);
-		FlammableBlockRegistry.getDefaultInstance().add(MANGROVE_BARK, 30, 60);
+		FlammableBlockRegistry.getDefaultInstance().add(OAK_BARK,		30, 60);
+		FlammableBlockRegistry.getDefaultInstance().add(SPRUCE_BARK,	30, 60);
+		FlammableBlockRegistry.getDefaultInstance().add(BIRCH_BARK,		30, 60);
+		FlammableBlockRegistry.getDefaultInstance().add(JUNGLE_BARK,	30, 60);
+		FlammableBlockRegistry.getDefaultInstance().add(ACACIA_BARK,	30, 60);
+		FlammableBlockRegistry.getDefaultInstance().add(DARK_OAK_BARK,	30, 60);
+		FlammableBlockRegistry.getDefaultInstance().add(MANGROVE_BARK,	30, 60);
+
+		FuelRegistry.INSTANCE.add(OAK_BARK,			150);
+		FuelRegistry.INSTANCE.add(SPRUCE_BARK,		150);
+		FuelRegistry.INSTANCE.add(BIRCH_BARK,		150);
+		FuelRegistry.INSTANCE.add(JUNGLE_BARK,		150);
+		FuelRegistry.INSTANCE.add(ACACIA_BARK,		150);
+		FuelRegistry.INSTANCE.add(DARK_OAK_BARK,	150);
+		FuelRegistry.INSTANCE.add(MANGROVE_BARK,	150);
+
+		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register(content -> {
+			content.addAfter(Items.OAK_WOOD,		OAK_BARK);
+			content.addAfter(Items.SPRUCE_WOOD,		SPRUCE_BARK);
+			content.addAfter(Items.BIRCH_WOOD,		BIRCH_BARK);
+			content.addAfter(Items.JUNGLE_WOOD,		JUNGLE_BARK);
+			content.addAfter(Items.ACACIA_WOOD,		ACACIA_BARK);
+			content.addAfter(Items.DARK_OAK_WOOD,	DARK_OAK_BARK);
+			content.addAfter(Items.MANGROVE_WOOD,	MANGROVE_BARK);
+			content.addAfter(Items.CRIMSON_HYPHAE,	CRIMSON_ANNULUS);
+			content.addAfter(Items.WARPED_HYPHAE,	WARPED_ANNULUS);
+		});
 	}
 
 	private static Block register(String name, Block block) {
-		return (Block)Registry.register(Registry.BLOCK, new ResourceLocation(BarkBlocks.MOD_ID, name), block);
+		Registry.register(BuiltInRegistries.BLOCK, new ResourceLocation(BarkBlocks.MOD_ID, name), block);
+		Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(BarkBlocks.MOD_ID, name), new BlockItem(block, new Item.Properties()));
+		return block;
 	}
 }
