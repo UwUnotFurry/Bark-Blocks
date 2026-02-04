85e0b4f206bcd4a899f775c61d16830f5c84ad9d
UwUnotFurry <uwunotfurry+GitHub@gmail.com>
2026-02-04 14:42:14 +0000

    Updated for minecraft 1.21.2

--- src/main/java/uwunotfurry/bark_blocks/world/level/block/Blocks.java
+++ src/main/java/uwunotfurry/bark_blocks/world/level/block/Blocks.java
@@ -4,16 +4,15 @@ import java.util.function.Function;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
-import net.fabricmc.fabric.api.registry.FuelRegistry;
+import net.fabricmc.fabric.api.registry.FuelRegistryEvents;
 
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTabs;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 
 import uwunotfurry.bark_blocks.BarkBlocks;
 import uwunotfurry.bark_blocks.world.level.block.HollowRotatedPillarBlock;
@@ -22,63 +21,53 @@ public class Blocks {
 
 	public static final Block OAK_BARK = register(
 		"oak_bark",
-		new HollowRotatedPillarBlock(Block.Properties.ofFullCopy(
-			net.minecraft.world.level.block.Blocks.OAK_LOG)
-		)
+		HollowRotatedPillarBlock::new,
+		Block.Properties.ofFullCopy(net.minecraft.world.level.block.Blocks.OAK_LOG)
 	);
 	public static final Block SPRUCE_BARK = register(
 		"spruce_bark",
-		new HollowRotatedPillarBlock(Block.Properties.ofFullCopy(
-			net.minecraft.world.level.block.Blocks.SPRUCE_LOG)
-		)
+		HollowRotatedPillarBlock::new,
+		Block.Properties.ofFullCopy(net.minecraft.world.level.block.Blocks.SPRUCE_LOG)
 	);
 	public static final Block BIRCH_BARK = register(
 		"birch_bark",
-		new HollowRotatedPillarBlock(Block.Properties.ofFullCopy(
-			net.minecraft.world.level.block.Blocks.BIRCH_LOG)
-		)
+		HollowRotatedPillarBlock::new,
+		Block.Properties.ofFullCopy(net.minecraft.world.level.block.Blocks.BIRCH_LOG)
 	);
 	public static final Block JUNGLE_BARK = register(
 		"jungle_bark",
-		new HollowRotatedPillarBlock(Block.Properties.ofFullCopy(
-			net.minecraft.world.level.block.Blocks.JUNGLE_LOG)
-		)
+		HollowRotatedPillarBlock::new,
+		Block.Properties.ofFullCopy(net.minecraft.world.level.block.Blocks.JUNGLE_LOG)
 	);
 	public static final Block ACACIA_BARK = register(
 		"acacia_bark",
-		new HollowRotatedPillarBlock(Block.Properties.ofFullCopy(
-			net.minecraft.world.level.block.Blocks.ACACIA_LOG)
-		)
+		HollowRotatedPillarBlock::new,
+		Block.Properties.ofFullCopy(net.minecraft.world.level.block.Blocks.ACACIA_LOG)
 	);
 	public static final Block DARK_OAK_BARK = register(
 		"dark_oak_bark",
-		new HollowRotatedPillarBlock(Block.Properties.ofFullCopy(
-			net.minecraft.world.level.block.Blocks.DARK_OAK_LOG)
-		)
+		HollowRotatedPillarBlock::new,
+		Block.Properties.ofFullCopy(net.minecraft.world.level.block.Blocks.DARK_OAK_LOG)
 	);
 	public static final Block MANGROVE_BARK = register(
 		"mangrove_bark",
-		new HollowRotatedPillarBlock(Block.Properties.ofFullCopy(
-			net.minecraft.world.level.block.Blocks.MANGROVE_LOG)
-		)
+		HollowRotatedPillarBlock::new,
+		Block.Properties.ofFullCopy(net.minecraft.world.level.block.Blocks.MANGROVE_LOG)
 	);
 	public static final Block CHERRY_BARK = register(
 		"cherry_bark",
-		new HollowRotatedPillarBlock(Block.Properties.ofFullCopy(
-			net.minecraft.world.level.block.Blocks.CHERRY_LOG)
-		)
+		HollowRotatedPillarBlock::new,
+		Block.Properties.ofFullCopy(net.minecraft.world.level.block.Blocks.CHERRY_LOG)
 	);
 	public static final Block CRIMSON_ANNULUS = register(
 		"crimson_annulus",
-		new HollowRotatedPillarBlock(Block.Properties.ofFullCopy(
-			net.minecraft.world.level.block.Blocks.CRIMSON_STEM)
-		)
+		HollowRotatedPillarBlock::new,
+		Block.Properties.ofFullCopy(net.minecraft.world.level.block.Blocks.CRIMSON_STEM)
 	);
 	public static final Block WARPED_ANNULUS = register(
 		"warped_annulus",
-		new HollowRotatedPillarBlock(Block.Properties.ofFullCopy(
-			net.minecraft.world.level.block.Blocks.WARPED_STEM)
-		)
+		HollowRotatedPillarBlock::new,
+		Block.Properties.ofFullCopy(net.minecraft.world.level.block.Blocks.WARPED_STEM)
 	);
 
 	public static void onInitialise() {
@@ -91,14 +80,16 @@ public class Blocks {
 		FlammableBlockRegistry.getDefaultInstance().add(MANGROVE_BARK,	30, 60);
 		FlammableBlockRegistry.getDefaultInstance().add(CHERRY_BARK,	30, 60);
 
-		FuelRegistry.INSTANCE.add(OAK_BARK,			150);
-		FuelRegistry.INSTANCE.add(SPRUCE_BARK,		150);
-		FuelRegistry.INSTANCE.add(BIRCH_BARK,		150);
-		FuelRegistry.INSTANCE.add(JUNGLE_BARK,		150);
-		FuelRegistry.INSTANCE.add(ACACIA_BARK,		150);
-		FuelRegistry.INSTANCE.add(DARK_OAK_BARK,	150);
-		FuelRegistry.INSTANCE.add(MANGROVE_BARK,	150);
-		FuelRegistry.INSTANCE.add(CHERRY_BARK,		150);
+		FuelRegistryEvents.BUILD.register((builder, context) -> {
+			builder.add(OAK_BARK,		150);
+			builder.add(SPRUCE_BARK,	150);
+			builder.add(BIRCH_BARK,		150);
+			builder.add(JUNGLE_BARK,	150);
+			builder.add(ACACIA_BARK,	150);
+			builder.add(DARK_OAK_BARK,	150);
+			builder.add(MANGROVE_BARK,	150);
+			builder.add(CHERRY_BARK,	150);
+		});
 
 		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register(content -> {
 			content.addAfter(Items.OAK_WOOD,		OAK_BARK);
@@ -114,9 +105,9 @@ public class Blocks {
 		});
 	}
 
-	private static Block register(String name, Block block) {
-		Registry.register(BuiltInRegistries.BLOCK, ResourceLocation.fromNamespaceAndPath(BarkBlocks.MOD_ID, name), block);
-		Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(BarkBlocks.MOD_ID, name), new BlockItem(block, new Item.Properties()));
+	private static Block register(String name, Function<BlockBehaviour.Properties, Block> blockFactory, BlockBehaviour.Properties properties) {
+		Block block = net.minecraft.world.level.block.Blocks.register(ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(BarkBlocks.MOD_ID, name)), blockFactory, properties);
+		Items.registerBlock(block);
 		return block;
 	}
 }
